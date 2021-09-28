493274a0f1144fea9d0cb215d3543833fb466630
ntukirafang <61813818+ntukirafang@users.noreply.github.com>
2021-09-28 13:25:55 +0800

    Update GSEA.r

--- GSEA.r
+++ GSEA.r
@@ -1,33 +1,37 @@
-#GSEA and metabolism
+#GSEA and metabolism 
+# install necessary package
 if (!requireNamespace("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
 BiocManager::install("apeglm")
-library(dplyr)
-library(tibble)
-library("AnnotationDbi")
 if (!requireNamespace("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
-library("AnnotationDbi")
-library("org.Mm.eg.db")
+BiocManager::install("pathview")
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
 if (!requireNamespace("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
 BiocManager::install("clusterProfiler")
+library(dplyr)
+library(tibble)
+library("AnnotationDbi")
 library(clusterProfiler)
-keytypes(org.Mm.eg.db)
+library("org.Mm.eg.db")
+library("org.Hs.eg.db")
 require(DOSE)
 DOSE::dotplot
 library(enrichplot)
-if (!requireNamespace("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
-
-BiocManager::install("pathview")
 library(pathview)
+# adjust deviation 
 dd2 <- lfcShrink(dds, contrast=contrast, res=dd1,type="apeglm")
 plotMA(dd2, ylim=c(-2,2))
+# Deviation analysis
 summary(dd1, alpha = 1)
-res <- dd2 %>% 
+# Filter Na value
+res <- res %>% 
   data.frame() %>% 
-  rownames_to_column("gene_id")
+# Gene Id add to column
+rownames_to_column("gene_id")
+# plot specific feature between control and treat
 ggplot2(data_plot, aes(condition,"ENSG00000206503",fill=condition)) +
   geom_boxplot() +
   geom_point(size=2, alpha=0.5) +
@@ -35,11 +39,11 @@ ggplot2(data_plot, aes(condition,"ENSG00000206503",fill=condition)) +
   xlab("") +
   ylab(paste("Expression of ","ENSG00000206503"))+
   theme_classic()+
-  theme(legend.position = "none")
-  res <- res %>% 
-  data.frame() %>% 
-  rownames_to_column("symbol")
-  res$ENSEMBL <- mapIds(org.Mm.eg.db,
+  theme(legend.position = "none") 
+# add symbol name to column
+rownames_to_column("symbol")
+# Exchange Gene ID to ensemble name and entrez 
+res$ENSEMBL <- mapIds(org.Mm.eg.db,
                      keys=res$symbol,
                      column="ENSEMBL",
                      keytype="SYMBOL",
@@ -49,20 +53,28 @@ res$entrez <- mapIds(org.Mm.eg.db,
                      column="ENTREZID",
                      keytype="SYMBOL",
                      multiVals="first")
+# Filter off Na value after exchange 
 gene_df <- res %>% 
 dplyr::select(symbol,log2FoldChange,ENSEMBL,entrez) %>% 
 filter(entrez!="NA") %>% 
 distinct(entrez,.keep_all = T)
+# Log foldchange with gene name
 geneList <- gene_df$log2FoldChange
 view(geneList)
+# Name as entraz and ensemble
 names(geneList) = gene_df$entrez
 names(geneList) = gene_df$ENSEMBL
+# Sorting with gen list
 geneList = sort(geneList, decreasing = TRUE)
 head(geneList)
+# Write ouput result in csv 
 write.csv(as.data.frame(gene_df), file=paste(directory,"gene_df.csv"))
 gene_df_output<- read.csv(paste(directory,"gene_df.csv"))
 write.csv(as.data.frame(geneList), file=paste(directory,"geneList.csv"))
 geneList_output<- read.csv(paste(directory,"geneList.csv"))
+# Set reference species in Mouse
+keytypes(org.Mm.eg.db)
+# Run out KEGG pathway result
 gseaKEGG <- gseKEGG(geneList     = geneList,
                     organism     = 'mmu',
                     nPerm        = 1000,
@@ -88,6 +100,7 @@ kk2 <- gseKEGG(organism     = geneList_organism,
                maxGSSize    = 800,
                pvalueCutoff = 0.05,
                pAdjustMethod = "none")
+# Run out gene ontology result
 kk2 <- gseGO(geneList=geneList, 
              ont ="ALL", 
              nPerm = 10000, 
@@ -108,7 +121,7 @@ gse <- gseGO(geneList=geneList,
              verbose = TRUE, 
              OrgDb = org.Mm.eg.db, 
              pAdjustMethod = "none")
-
+# Run out pathway
 gsepathway <- gseKEGG(geneList=geneList, 
              organism = "mmu",
              nPerm = 10000, 
@@ -117,13 +130,17 @@ gsepathway <- gseKEGG(geneList=geneList,
              pvalueCutoff = 0.99, 
              verbose = TRUE, 
              pAdjustMethod = "none")
+# dotplot show top 50 pathway(Up and down)
 dotplot(kk2,showCategory=50,split=".sign")+facet_grid(~.sign)
+# Emission plot show top 50 pathway
 emapplot(kk2, showCategory = 50)
+# Central plot show top 20
 cnetplot(kk2, showCategory = 20)
 dotplot(gse,showCategory=20,split=".sign")+facet_grid(~.sign)
 emapplot(gse, showCategory = 50)
 cnetplot(gse, showCategory = 20) 
 dotplot(gseaKEGG,showCategory=20,split=".sign")+facet_grid(~.sign)
+# Write dataframe in csv 
 gseaKEGG_results <- gseaKEGG@result
 write.csv(as.data.frame(gseaKEGG_results), file=paste(directory,"gseaKEGG_results.csv"))
 gseaKEGG_output<- read.csv(paste(directory,"gseaKEGG_results.csv"))
@@ -136,13 +153,15 @@ gsepathway_output<- read.csv(paste(directory,"gsepathway_results.csv"))
 gse_results <- gse@result
 write.csv(as.data.frame(gse_results), file=paste(directory,"gse_results.csv"))
 gse_output<- read.csv(paste(directory,"gse_results.csv"))
+# Plot specific pathway GSEA
 pathway.id = "mmu04215"
 gseaplot2(gsepathway,color = "red",geneSetID = pathway.id,pvalue_table = T)
 pathway.id = "mmu04215"
+# Show KEGG pathway (upregulation gene and downregulation gene in pathway) 
 pv.out <- pathview(gene.data  = geneList,
                    pathway.id = pathway.id,
                    species    = "mmu")
-library(pathview)
+
 pathway.id = "mmu04146"
 pv.out <- pathview(gene.data  = geneList,
                    pathway.id = pathway.id,
